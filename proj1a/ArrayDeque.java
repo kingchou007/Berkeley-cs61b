f8b1f3722be049eec85f52834b9f8e73113e03ee
Kane <kingchou007@gmail.com>
2022-05-07 19:37:32 -0400

    ch3 study notes

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,8 +1,29 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+
+    private int size;
+    private T[] items;
+
+    public ArrayDeque(){
+        size = 0;
+        items = (T[]) new Object[8]; // 不懂
+    }
+
 
     public ArrayDeque(ArrayDeque other){
 
     }
+
+
+
+
+
+    public int size(){
+        return size;
+    }
+
+
+
+
 
 
 
